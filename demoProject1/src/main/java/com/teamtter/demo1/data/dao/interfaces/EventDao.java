d3b65000602dc8b13f5d352fddc1e25cea28900f
fmarot <francois.marot@gmail.com>
2011-04-21 23:47:21 +0200

    added files

---
+++ demoProject1/src/main/java/com/teamtter/demo1/data/dao/interfaces/EventDao.java
@@ -0,0 +1,10 @@
+package com.teamtter.demo1.data.dao.interfaces;
+
+import com.teamtter.demo1.data.dataobjects.Event;
+/**
+ * @author Richard Wilkinson - richard.wilkinson@jweekend.com
+ *
+ */
+public interface EventDao extends Dao<Event> {
+
+}
